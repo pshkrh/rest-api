0cf2aa7154a08a8e231224d395f8092ef640d666
Pushkar Kurhekar <pshkrh@gmail.com>
2018-10-13 00:47:36 +0530

    Change order model to accept only the order number

--- models/order.py
+++ models/order.py
@@ -5,24 +5,14 @@ class OrderModel(db.Model):
     __tablename__ = 'orders'
 
     order_no = db.Column(db.Integer, primary_key=True)
-    product_code = db.Column(db.String(80))
-    name = db.Column(db.String(80))
-    quantity = db.Column(db.String(80))
-    price = db.Column(db.String(80))
-    delivery_date = db.Column(db.String(80))
-    currency = db.Column(db.String(80))
+    items = db.relationship('ItemModel', lazy='dynamic')
+    
 
-    def __init__(self,order_no,product_code,name,quantity,price,delivery_date,currency):
+    def __init__(self,order_no):
         self.order_no = order_no
-        self.product_code = product_code
-        self.name = name
-        self.quantity = quantity
-        self.price = price
-        self.delivery_date = delivery_date
-        self.currency = currency
 
     def json(self):
-        return {'order_no': self.order_no, 'product_code': self.product_code, 'name': self.name, 'quantity': self.quantity, 'price': self.price,'delivery_date': self.delivery_date, 'currency': self.currency}   
+        return {'order_no': self.order_no, 'items': [item.json() for item in self.items.all()]}   
 
     @classmethod
     def find_by_order_no(cls,order_no):
